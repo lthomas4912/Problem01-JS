ae6f0520a751c719da4614248fcc9ba3faea9965
leah4912 <leah29726@gmail.com>
2020-09-22 19:58:54 -0400

    passed test

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,8 +1,30 @@
 class WuTangForeverBuzz {
-    wuTangClan(value){
-        let output ="";
+    wuTangClan(value) {
+
+        let output = "";
+        for (let i = 1; i <= value; i++) {
+
+            if (i % 15 == 0) {
+                output +="WuTangForever\n";
+
+            }
+             
+            else if (i % 3 == 0) {
+                output += "Wu\n";
+            } 
+            
+            else if (i % 5 == 0) {
+
+               output += "Tang\n";
+            } else{
+                output += i + "\n";
+            }
+ 
+
+        }
 
         return output;
+
     }
 }
 
